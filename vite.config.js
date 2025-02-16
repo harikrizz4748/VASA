145ec34e63d13f0716fca97d1ba96bf38284015b
harikrizz4748 <harikrishnanmulecraft@gmail.com>
2025-02-16 16:17:23 +0000

    update

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from "path"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { tempo } from "tempo-devtools/dist/vite"; // Add this import
+
+// Add this block of code
+const conditionalPlugins = [];
+if (process.env.TEMPO) {
+  conditionalPlugins.push(["tempo-devtools/swc", {}]);
+}
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/SnapLogicPlayground1/',
+  base: "/SnapLogicPlayground1/",
   build: {
-    outDir: 'dist',
+    outDir: "dist",
   },
-
-  plugins: [react()],
-  
+  plugins: [
+    react({
+      plugins: [...conditionalPlugins], // Add the conditional plugin
+    }),
+    tempo(), // Add the tempo plugin
+  ],
+  server: {
+    // @ts-ignore
+    allowedHosts: process.env.TEMPO === "true" ? true : undefined, // Whatever was the previous value or undefined
+  },
   define: {
-    'process.env': {},
+    "process.env": {},
   },
 
   resolve: {
@@ -20,4 +34,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
